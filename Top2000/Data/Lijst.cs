e7c6d4d0da4ac0676e8ad498bdf67363661e9e3b
JTenhagen <j.tenhagen@hotmail.com>
2017-06-12 10:27:55 +0200

    classes dataset

--- Top2000/Data/Lijst.cs
+++ Top2000/Data/Lijst.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
+using System.Windows;
 
 namespace Data
 {
@@ -9,5 +12,46 @@ namespace Data
     {
         private int top2000jaar;
         private int positie;
+
+        public static List<Song> ListSong = new List<Song>();
+        public static List<Artiest> ListArtiest = new List<Artiest>();
+
+        public static void AddArtiest()
+        {
+            DataTable table = new DataTable();
+            SqlConnection conn = new SqlConnection();
+            SqlCommand cmd;
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(@"Server=(localdb)\mssqllocaldb;");
+            sb.Append("Database=TOP2000;");
+            sb.Append("User Id=I5AO1;  Password=test;");
+
+            string cs = sb.ToString();
+
+            conn.ConnectionString = cs;
+
+            try
+            {
+                conn.Open();
+                cmd = new SqlCommand("SELECT * FROM Artiest", conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+                table.Load(reader);
+                
+                foreach (var v in table.Rows.Count.ToString())
+                {
+                    //ListArtiest.Add(new Artiest(table.Rows.));
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+            catch (Exception ex1)
+            {
+                MessageBox.Show(ex1.Message);
+            }
+        }
     }
 }
